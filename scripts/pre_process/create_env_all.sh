322c1571a5a30b5bf6599e06657f9d47cad60053
fabioluiztx <fabs.375@gmail.com>
2023-04-02 17:43:12 -0300

    Alteração no script create_env.sh

--- scripts/pre_process/create_env_all.sh
+++ scripts/pre_process/create_env_all.sh
@@ -2,14 +2,18 @@
 
 # Criação das pastas
 
-DADOS=("cidade" "estado" "filial" "parceiro" "cliente" "subcategoria" "categoria" "item_pedido" "produto")
+DADOS=("vendas" "clientes" "endereco" "regiao" "divisao")
+
+echo "Criando a estrutura de pastas no hdfs"
 
 for i in "${DADOS[@]}"
 do
-	echo "$i"
+	echo "Criando a pasta datalake/raw/$i"
     cd ../../raw/
     hdfs dfs -mkdir /datalake/raw/$i
     hdfs dfs -chmod 777 /datalake/raw/$i
+	
+	echo "Enviando arquivo $i para datalake/raw/$i"
     hdfs dfs -copyFromLocal $i.csv /datalake/raw/$i
     beeline -u jdbc:hive2://localhost:10000 -f ../../scripts/hql/create_table_$i.hql 
 done
