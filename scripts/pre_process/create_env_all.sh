ebdb739203cac2b2d6d3aaba86e88dead9ab3150
Fabio Teixeira <fabs.375@gmail.com>
2023-04-02 23:26:40 +0000

    "Alteração no script para manipulação da tabela"

--- scripts/pre_process/create_env_all.sh
+++ scripts/pre_process/create_env_all.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+#Trocando os espaços em brancos do header por underline ("_") para manipulação das tabelas no hive
+TABELAS=("CLIENTES" "DIVISAO" "ENDERECO" "REGIAO" "VENDAS")
+
+for i in "${TABELAS[@]}"
+
+do
+    echo "Alterando o header das colunas do $i.CSV para manipulação"
+    chmod 777 $i.csv
+    sed -i '1,1s/ /_/g' $i.csv
+
+done
+
 # Criação das pastas
 
 DADOS=("vendas" "clientes" "endereco" "regiao" "divisao")
@@ -16,4 +28,5 @@ do
 	echo "Enviando arquivo $i para datalake/raw/$i"
     hdfs dfs -copyFromLocal $i.csv /datalake/raw/$i
     beeline -u jdbc:hive2://localhost:10000 -f ../../scripts/hql/create_table_$i.hql 
+    
 done
