ab1aa190f09e4f0e17ec94d83a1e971e8655f169
Fabio Teixeira <fabs.375@gmail.com>
2023-04-03 14:44:51 +0000

    "Atualizando script com comentários"

--- scripts/pre_process/create_env_all.sh
+++ scripts/pre_process/create_env_all.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 #Trocando os espaços em brancos do header por underline ("_") para manipulação das tabelas no hive
+#e colocando em upper case pra bater com o hql modificado
+
 TABELAS=("CLIENTES" "DIVISAO" "ENDERECO" "REGIAO" "VENDAS")
 
-for i in "${TABELAS[@]}"
+#Percorrendo as tabelas
+for j in "${TABELAS[@]}"
 
 do
     echo "Alterando o header das colunas do $i.CSV para manipulação"
-    chmod 777 $i.csv
-    sed -i '1,1s/ /_/g' $i.csv
+    chmod 777 $j.csv #dando permissão para modificar o arquivo
+    sed -i '1,1s/ /_/g' $j.csv #percorrendo apenas a primerira linha na coluna e alterando o espaço por underline (_)
+    sed -i '1,1s/[A-Z]/\L&/g' $j.csv #colocando tudo em letra minúscula pra facilitar a manipulação no hive
+    
 
 done
 
@@ -27,6 +32,6 @@ do
 	
 	echo "Enviando arquivo $i para datalake/raw/$i"
     hdfs dfs -copyFromLocal $i.csv /datalake/raw/$i
-    beeline -u jdbc:hive2://localhost:10000 -f ../../scripts/hql/create_table_$i.hql 
+    beeline -u jdbc:hive2://localhost:10000 hive -f ../../scripts/hql/create_table_$i.hql 
     
 done
